bdf8f57a84be9355a22071216ecca7fc37a6ba43
A <hanxuepu@qq.com>
2020-12-15 14:09:01 +0800

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations22222222
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site!</span>
         <span role="img" aria-label="Party popper emojis">
